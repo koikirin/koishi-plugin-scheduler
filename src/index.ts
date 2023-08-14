307da561b010a64fe64b039454190777b84f5709
Hieuzest <girkirin@hotmail.com>
2023-08-14 20:58:33 +0800

    feat: scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,55 @@
-import { Context, Schema } from 'koishi'
+import { Awaitable, Context, Service } from 'koishi'
+import { parseExpression } from 'cron-parser'
 
-export const name = 'scheduler'
-
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  // write your plugin here
+declare module 'koishi' {
+  interface Context {
+    scheduler: Scheduler
+  }
 }
+
+export class Scheduler extends Service {
+  constructor(ctx: Context) {
+    super(ctx, 'scheduler')
+  }
+
+  every(ms: number, callback: () => Awaitable<void>) {
+    const timer = setInterval(callback, ms)
+    return this.caller.collect('scheduler', () => (clearInterval(timer), true))
+  }
+
+  next(ms: number, callback: () => Awaitable<void>) {
+    const timeout = setTimeout(callback, ms)
+    return this.caller.collect('scheduler', () => (clearTimeout(timeout), true))
+  }
+
+  at(time: number | { getTime(): number }, callback: () => Awaitable<void>) {
+    if (typeof time === 'number') return this.next(time - Date.now(), callback)
+    else return this.next(time.getTime() - Date.now(), callback)
+  }
+
+  cron(crontab: string, callback: () => Awaitable<void>, startDate?: Date, endDate?: Date) {
+    const cronitor = this.cronitor(crontab, startDate, endDate)
+    let flag = true
+    const task = () => {
+      if (!flag) return
+      callback()
+      let next = cronitor.next()
+      if (!next.done) this.at(next.value, task)
+    }
+    let next = cronitor.next()
+    if (!next.done) {
+      const timeout = setTimeout(task, next.value.getTime() - Date.now())
+      return this.caller.collect('scheduler', () => (clearTimeout(timeout), flag = false, true))
+    }
+  }
+
+  private cronitor(crontab: string, startDate?: Date, endDate?: Date) {
+    return parseExpression(crontab, {
+      startDate,
+      endDate,
+      iterator: true
+    })
+  }
+}
+
+export default Scheduler
